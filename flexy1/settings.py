82f0e192c49f691aabd32ef73880fb72fde9dd3b
Pavel Bekoev <pablank@bk.ru>
2019-11-27 19:02:49 +0300

    ss

--- flexy1/settings.py
+++ flexy1/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 #ALLOWED_HOSTS = []
 
 #ALLOWED_HOSTS = ('*')
-ALLOWED_HOSTS = ['intense-woodland-79843.herokuapp.com/','127.0.0.1:8000']
+ALLOWED_HOSTS = ['intense-woodland-79843.herokuapp.com','127.0.0.1:8000']
 
 
 
